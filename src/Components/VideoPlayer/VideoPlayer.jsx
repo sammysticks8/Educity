1be1b1eae0b5ee9ea257ddbaddcbd27d6efd01f7
sammysticks8 <sammysticks82@gmail.com>
2024-03-27 13:27:28 -0700

    educational reactjs

---
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './VideoPlayer.css'
+// import video from ''
+
+const VideoPlayer = () => {
+  return (
+    <div className='video-player hide'>
+     {/* <video src={video} autoPlay muted controls></video> */}
+    </div>
+  ) 
+}
+
+export default VideoPlayer
